e30700526ddf62638351eca280ef7c8fba305ce5
mdulgeroglu <Desktop@ZET.local>
2015-11-12 13:28:07 -0500

    database test

--- test/database.js
+++ test/database.js
@@ -2,14 +2,40 @@ var db = require('../lib/Database');
 
 
 describe('lib/Database', function(){
+
 	it('get root article by parent id', function(done){
 		db.getList(0, function(err, data){
 			if(err) throw err;
-			if(data.length == 0) throw new Error('there shold be some articles');
+			if(data.length == 0) throw new Error('database error: there shold be some articles');
 			done();
 		})
 	});
-	it('get root article by path');
-	it('get article by id');
-	it('get article by url');
+
+    
+	it('get root article by path', function(done){
+		db.getListByPath('/nur-lectures', function(err, data){
+			if(err) throw err;
+			if(data.length == 0) throw new Error('database error: there should be a path');
+			done();
+		})
+	});
+
+    
+    it('get article by id', function(done){
+		db.getArticle(40, function(err, data){
+			if(err) throw err;
+			if(data.length == 0) throw new Error('database error: cannot find article');
+			done();
+		})
+	});
+
+    
+	it('get article by url', function(done){
+		db.getArticleByURL('/nur-lectures/video', function(err, data){
+			if(err) throw err;
+			if(data.length == 0) throw new Error('database error: cannot find url');
+			done();
+		})
+	});
+    
 })
